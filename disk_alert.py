a90a5694c90c777e58688661837b768ca85de298
Leonardo Astros <leonardo@vauxoo.com>
2015-03-04 16:39:03 -0430

    [ADD] Parser added

--- disk_alert.py
+++ disk_alert.py
@@ -2,26 +2,41 @@
 # -*- coding: utf-8 -*-
 
 import smtplib
+import argparse
 from sh import df
+from sh import uname
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--server", help="SMTP Server",
+                    default="smtp.gmail.com:587")
+parser.add_argument("-F", "--From", help="Sender's email address",
+                    required=True)
+parser.add_argument("-T", "--To", help="Comma separated recipient's email list",
+                    required=True)
+parser.add_argument("-W", "--pswd", help="Email password", required=True)
 
-def sendmail(from_add, to_add, subject, message, login, psswd,
-             smtpserver='smtp.gmail.com:587', cc_add=None):
-    header = 'From: %s\n' % from_add
-    header += 'To: %s\n' % ','.join(to_add)
-    if cc_add:
-        header += 'Cc: %s\n' % ','.join(cc_add)
-    header += 'Subject: %s\n\n' % subject
-    message = header + message
-    server = smtplib.SMTP(smtpserver)
+args = parser.parse_args()
+mail_info = {'server': args.server,
+             'from': args.From,
+             'login': args.From,
+             'to': args.To.split(","),
+             'pswd': args.pswd
+}
+
+def sendmail(info):
+    header = "From: %s\n" % info['from']
+    header += "To: %s\n" % ",".join(info['to'])
+    header += "Subject: %s\n\n" % info['subject']
+    message = header + info['message']
+    server = smtplib.SMTP(info['server'])
     server.starttls()
-    server.login(login, psswd)
-    problems = server.sendmail(from_add, to_add, message)
+    server.login(info['login'], info['pswd'])
+    problems = server.sendmail(info['from'], info['to'], message)
     server.quit()
 
 def free_space():
-    out_df = df(block_size='G')
-    out_df = out_df.encode('utf-8', 'replace')
+    out_df = df(block_size="G")
+    out_df = out_df.encode("utf-8", "replace")
     data = out_df.split(" ")
     while data.count("") > 0:
         data.remove("")
@@ -31,10 +46,7 @@ def free_space():
     return int(data[id_free].split("G")[0])
 
 
-sendmail(from_add='python@RC.net',
-         to_add=['leonardo@vauxoo.com'],
-         cc_add=['tulio@vauxoo.com'],
-         subject='Prueba',
-         message='Message',
-         login='el_correo_@vauxoo.com',
-         psswd='pswd')
+if __name__=='__main__':
+    mail_info.update({'subject': "Prueba"})
+    mail_info.update({'message': str(uname(nodename=True))})
+    sendmail(mail_info)
