519ca510ee9c4f8f0dee28e5d1862aae39fa0407
Leonardo Astros <leonardo@vauxoo.com>
2015-07-13 02:43:58 -0430

    [ADD] Adding take most recent backup file

--- test_db.py
+++ test_db.py
@@ -5,6 +5,7 @@ backup files
 """
 
 import os
+import datetime
 import logging
 import argparse
 
@@ -12,8 +13,10 @@ from lib import utils
 from deactivate_ws import deactivate
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-f", "--backup-file", help="Path of backup file",
-                    required=True)
+action = parser.add_mutually_exclusive_group(required=True)
+action.add_argument("-f", "--backup-file", help="Path of backup file")
+action.add_argument("-p", "--backup-path",
+                    help="Path of backup dir. Most recent backup will be restored")
 parser.add_argument("--log-level", help="Level of logger. INFO as default",
                     default="info")
 parser.add_argument("--logfile", help="File where log will be saved",
@@ -31,9 +34,32 @@ logging.basicConfig(level=level,
 logger = logging.getLogger("testbd_creator")
 
 db_file = args.backup_file
+path = args.backup_path
 json_file = args.config_file
 config = args.config
 tmp = args.temp_dir
+
+
+def get_date(db_file):
+    date_str = os.path.basename(db_file).split(".")[0]
+    str_list = date_str.split("-")
+    str_list = str_list[len(str_list) - 1].split("_")
+    date_str = str_list[len(str_list) - 2]
+    time_str = str_list[len(str_list) - 1]
+    try:
+        year = int(date_str[0:4])
+        month = int(date_str[4:6])
+        day = int(date_str[6:])
+        hour = int(time_str[0:2])
+        minute = int(time_str[2:4])
+        second = int(time_str[4:])
+        file_date = datetime.datetime(year=year, month=month, day=day,
+                                      hour=hour, minute=minute, second=second)
+    except Exception as e:
+        file_date = datetime.datetime(year=datetime.MINYEAR, month=1, day=1,
+                                      hour=0, minute=0, second=0)
+        logger.error("File: %s - %s", os.path.basename(db_file), e)
+    return file_date
 
 
 def restore_database(db_name, db_config, dump_dest, port):
@@ -68,6 +94,7 @@ def restore_database(db_name, db_config, dump_dest, port):
         return 1
     return db_name
 
+
 def create_test_db(prefix, db_file, db_config, temp_dir):
     """
     This function creates a database from backup file with determined config
@@ -89,7 +116,18 @@ def create_test_db(prefix, db_file, db_config, temp_dir):
     return result
 
 
+def select_file(path):
+    logger.debug("Selecting backup file to be restored")
+    db_select = os.listdir(path)[0]
+    for dfile in os.listdir(path):
+        if get_date(dfile) > get_date(db_select):
+            db_select = dfile
+    logger.info("File %s selected", os.path.basename(db_select))
+    return db_select
+
 if __name__ == '__main__':
+    if not db_file:
+        db_file = select_file(path)
     logger.info("Creating %s database from %s backup", config, db_file)
     db_config = utils.load_json(json_file)
     db = create_test_db(config, db_file, db_config[config], tmp)
